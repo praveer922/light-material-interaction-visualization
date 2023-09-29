7cdea92e881c859c8beeb07693ff8e64a3649547
Praveer Tewari <praveer@xxs-MacBook-Pro.local>
2023-09-28 19:27:19 -0600

    add canvas element for webgl window

---
+++ js/render.js
@@ -0,0 +1,11 @@
+// Get a reference to the canvas element
+const canvas = document.getElementById('webgl-canvas');
+
+// Initialize WebGL context
+const gl = canvas.getContext('webgl');
+
+if (!gl) {
+  console.error('WebGL is not supported by your browser.');
+} else {
+  // WebGL code goes here
+}
