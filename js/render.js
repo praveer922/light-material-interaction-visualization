fbeb9de7e0c8095ad6fc96293c1835032fcd9aca
Praveer Tewari <praveer@xxs-MacBook-Pro.local>
2023-09-28 19:49:17 -0600

    add input form and stylesheet

--- js/render.js
+++ js/render.js
@@ -1,7 +1,22 @@
-// Get a reference to the canvas element
-const canvas = document.getElementById('webgl-canvas');
+// Listen for user input
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+  
+    // Access the input field value
+    var userInput = document.getElementById("textInput").value;
+  
+    // Call a function or perform actions with userInput
+    processUserInput(userInput);
+  });
+
+  function processUserInput(input) {
+    // TODO: for wenjian, do what you want with the user input here.
+    console.log("User input: " + input);
+  }
+
 
 // Initialize WebGL context
+const canvas = document.getElementById('webgl-canvas');
 const gl = canvas.getContext('webgl');
 
 if (!gl) {
