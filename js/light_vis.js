c30c193b4b78643ed15659bb7c5590046c3e27e3
H5uan <56057003+H5uan@users.noreply.github.com>
2023-11-24 22:31:54 -0700

    update logic of sampling visualization

--- js/light_vis.js
+++ js/light_vis.js
@@ -2,14 +2,17 @@ import * as THREE from 'three';
 
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {GUI} from "dat.gui";
+import {GetRandomSample, SampleConductor, SampleDiffuse} from "./sample";
+import {update} from "three/addons/libs/tween.module";
 
-let group, camera, scene, renderer, arrow;
+let group, camera, scene, renderer, arrow, hemisphere;
 
 const guiControls = {
     sphereColor: 0xffff00, // Initial color of the sphere
     arrowRadius: 20,      // Initial radius of the arrow's position
     arrowAzimuth: 0,      // Initial azimuthal angle (in degrees)
-    arrowPolar: 45        // Initial polar angle (in degrees)
+    arrowPolar: 45,        // Initial polar angle (in degrees)
+    segments: 8,         // Number of segments of the sphere
 };
 
 init();
@@ -21,16 +24,15 @@ function init() {
 
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     const sceneElement = document.querySelector('.scene');
     sceneElement.appendChild(renderer.domElement);
-    renderer.setSize(sceneElement.clientWidth, sceneElement.clientHeight);
 
     const gui = new GUI();
-    const controlPanelElement = document.querySelector('.control-panel');
-    controlPanelElement.appendChild(gui.domElement);
     gui.addColor(guiControls, 'sphereColor').onChange((color) => {
         sphere.material.color.set(color);
     });
+    gui.add(guiControls, 'segments', [4, 8, 16, 32]).onChange(updateHemiSphere);
     gui.add(guiControls, 'arrowRadius', 5, 50).onChange(updateArrowPosition);
     gui.add(guiControls, 'arrowAzimuth', 0, 360).onChange(updateArrowPosition);
     gui.add(guiControls, 'arrowPolar', 0, 180).onChange(updateArrowPosition);
@@ -63,18 +65,22 @@ function init() {
 
     scene.add(new THREE.GridHelper(100, 10, 0x888888, 0x444444))
 
-    const geometry = new THREE.SphereGeometry(15, 32, 16, 0, Math.PI * 2, 0, Math.PI / 2);
-    const material = new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true});
-    const sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
-
     const dir = new THREE.Vector3(1, 0, 0); // Direction of the arrow
-    const origin = new THREE.Vector3(0, 0, 0);
+    const phi = THREE.MathUtils.degToRad(90 - guiControls.arrowPolar); // Convert polar angle to radians
+    const theta = THREE.MathUtils.degToRad(guiControls.arrowAzimuth);  // Convert azimuthal angle to radians
+    const arrowPosition = new THREE.Vector3(
+        guiControls.arrowRadius * Math.sin(phi) * Math.cos(theta),
+        guiControls.arrowRadius * Math.cos(phi),
+        guiControls.arrowRadius * Math.sin(phi) * Math.sin(theta)
+    );
     const length = 5;
     const hex = 0xff0000; // Color of the arrow
-    arrow = new THREE.ArrowHelper(dir, origin, length, hex);
+    arrow = new THREE.ArrowHelper(dir, arrowPosition, length, hex);
     updateArrowPosition();
     scene.add(arrow);
+
+    hemisphere = buildHemisphere(15, guiControls.segments);
+    scene.add(hemisphere);
 }
 
 function animate() {
@@ -103,3 +109,131 @@ function updateArrowPosition() {
     const direction = new THREE.Vector3().subVectors(new THREE.Vector3(0, 0, 0), arrow.position).normalize();
     arrow.setDirection(direction);
 }
+
+function buildHemisphere(radius, segments) {
+    const group = new THREE.Group();
+    const azimuthalStep = Math.PI * 2 / segments;
+    const polarStep = Math.PI / 2 / segments;
+
+    for (let phi = 0; phi < Math.PI * 2; phi += azimuthalStep) {
+        for (let theta = 0; theta < Math.PI / 2; theta += polarStep) {
+            const x = radius * Math.sin(theta) * Math.cos(phi);
+            const z = radius * Math.sin(theta) * Math.sin(phi);
+            const y = radius * Math.cos(theta);
+
+            // 创建长方体
+            const length = radius; // 长方体的长度
+            const boxGeometry = new THREE.BoxGeometry(0.1, 0.1, length); // 长方体的尺寸
+            const boxMaterial = new THREE.MeshBasicMaterial({color: 0x808080}); // 长方体的颜色
+            const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+
+            cube.position.set(x / 2, y / 2, z / 2);
+
+            cube.lookAt(new THREE.Vector3(x, y, z));
+
+            group.add(cube);
+        }
+    }
+
+    return group;
+}
+
+function updateHemiSphere() {
+    if (hemisphere) {
+        scene.remove(hemisphere);
+    }
+    hemisphere = buildHemisphere(15, guiControls.segments);
+    scene.add(hemisphere);
+}
+
+function updateDiffuse(segments, sampleCount = 10000) {
+    const azimuthalSegments = segments;
+    const polarSegments = segments;
+    let distribution = Array.from(Array(azimuthalSegments), () => new Array(polarSegments).fill(0));
+
+    const azimuthalFactor = azimuthalSegments / (2 * Math.PI);
+    const polarFactor = polarSegments / Math.PI;
+
+    for (let i = 0; i < sampleCount; i++) {
+        let u = new THREE.Vector2(Math.random(), Math.random());
+        let wi = SampleDiffuse(u);
+
+        let azimuthalAngle = Math.atan2(wi.y, wi.x);
+        let polarAngle = Math.acos(wi.z / wi.length());
+
+        let azimuthalIndex = Math.floor((azimuthalAngle + Math.PI) * azimuthalFactor) % azimuthalSegments;
+        let polarIndex = Math.floor(polarAngle * polarFactor);
+
+        distribution[azimuthalIndex][polarIndex]++;
+    }
+
+    for (let i = 0; i < azimuthalSegments; i++) {
+        for (let j = 0; j < polarSegments; j++) {
+            distribution[i][j] /= sampleCount;
+        }
+    }
+
+    return distribution;
+}
+
+function updateConductor(wo, segments, sampleCount = 10000) {
+    const azimuthalSegments = segments;
+    const polarSegments = segments;
+    let distribution = Array.from(Array(azimuthalSegments), () => new Array(polarSegments).fill(0));
+
+    const azimuthalFactor = azimuthalSegments / (2 * Math.PI);
+    const polarFactor = polarSegments / Math.PI;
+
+    for (let i = 0; i < sampleCount; i++) {
+        let u = new THREE.Vector2(Math.random(), Math.random());
+        let wi = SampleConductor(wo, u, 0.5, 0.5);
+
+        let azimuthalAngle = Math.atan2(wi.y, wi.x);
+        let polarAngle = Math.acos(wi.z / wi.length());
+
+        let azimuthalIndex = Math.floor((azimuthalAngle + Math.PI) * azimuthalFactor) % azimuthalSegments;
+        let polarIndex = Math.floor(polarAngle * polarFactor);
+
+        distribution[azimuthalIndex][polarIndex]++;
+    }
+
+    for (let i = 0; i < azimuthalSegments; i++) {
+        for (let j = 0; j < polarSegments; j++) {
+            distribution[i][j] /= sampleCount;
+        }
+    }
+
+    return distribution;
+}
+
+function updateDielectric(wo, segments, sampleCount = 10000) {
+    const azimuthalSegments = segments;
+    const polarSegments = segments;
+    let distribution = Array.from(Array(azimuthalSegments), () => new Array(polarSegments).fill(0));
+
+    const azimuthalFactor = azimuthalSegments / (2 * Math.PI);
+    const polarFactor = polarSegments / Math.PI;
+
+    for (let i = 0; i < sampleCount; i++) {
+        let u = new THREE.Vector2(Math.random(), Math.random());
+        let wi = SampleConductor(wo, u, 0.5, 0.5, 1., 1.5);
+
+        let azimuthalAngle = Math.atan2(wi.y, wi.x);
+        let polarAngle = Math.acos(wi.z / wi.length());
+
+        let azimuthalIndex = Math.floor((azimuthalAngle + Math.PI) * azimuthalFactor) % azimuthalSegments;
+        let polarIndex = Math.floor(polarAngle * polarFactor);
+
+        distribution[azimuthalIndex][polarIndex]++;
+    }
+
+    for (let i = 0; i < azimuthalSegments; i++) {
+        for (let j = 0; j < polarSegments; j++) {
+            distribution[i][j] /= sampleCount;
+        }
+    }
+
+    return distribution;
+}
+
+
