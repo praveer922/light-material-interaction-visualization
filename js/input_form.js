d312f9f443d4ef414f1353f5ee74127966111418
Praveer Tewari <praveer@xxs-MacBook-Pro.local>
2023-11-30 15:02:04 -0700

    set default and reorder UI

--- js/input_form.js
+++ js/input_form.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const materialForm = document.getElementById("materialForm");
+    processUserInput(0);
     
 
     materialForm.addEventListener("submit", function(event) {
